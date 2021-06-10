a56c8cfc1ff490bab53ca44b0b730a638e0341ce
guru-dev90 <73295357+guru-dev90@users.noreply.github.com>
2021-06-10 16:25:15 +0200

    after images path replace

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -5,7 +5,7 @@ export default function Loading({ src, ...restProps }) {
     return (
         <Spinner {...restProps}>
             <LockBody />
-            <Picture src={`/images/users/${src}.png`} />
+            <Picture src={`%PUBLIC_URL%/images/users/${src}.png`} />
         </Spinner>
     )
 }
